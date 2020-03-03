36736f91c715e9cab67a7afd25af799a86af8cfc
Bryce Meyer <bryce13950@gmail.com>
2020-03-03 14:52:06 +0100

    updated to illuminate version 7 final release

--- tests/Laravel6DateFormatMiddlewareTest.php
+++ tests/Laravel6DateFormatMiddlewareTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace PomeloProductions\Laravel6Middleware\Tests;
 
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use PHPUnit\Framework\TestCase;
 use PomeloProductions\Laravel6Middleware\Laravel6DateFormatMiddleware;
